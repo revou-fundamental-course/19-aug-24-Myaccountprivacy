e78c6ffe3c4981dd38d94de387fb828c93c24a4e
Fadlan <aria.fadlan@gmail.com>
2024-08-26 20:17:10 +0700

    modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 // Written by Fadlan Aria P.N.
+// This is a JavaScript file. This is... I don't know what this thing does.
+// TODO: FIX THIS THING!
+
+ let name = prompt("What is your name?" "");
+ document.getElementById("name").innerHTML = name;
